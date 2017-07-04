6cdf20b9631881d5da59acc6c659fc79b2455f85
marilena daquino <marilena.daquino2@unibo.it>
2017-07-04 16:26:03 +0200

    Min fix php

--- dataset.php
+++ dataset.php
@@ -140,47 +140,47 @@ if ($handle) {
 		//if(d("Affordance: Is playable?",$t))
 		//literal ( $ID, $ns . 'ontology/affordance/playable', (v("Affordance: Is playable?",$line) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Affordance: Is playable?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/playable' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/playable' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/playable', 'http://www.w3.org/2000/01/rdf-schema#label', 'Playable');
-
+		}
 		// "Purpose: Learning",
 		//if(d("Purpose: Learning",$t))
 		//literal ( $ID, $ns . 'ontology/purpose/learning', (v("Purpose: Learning",$line ) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Purpose: Learning",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasPurpose', $ns . 'ontology/purpose/learning' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasPurpose', $ns . 'ontology/purpose/learning' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/purpose/learning', 'http://www.w3.org/2000/01/rdf-schema#label', 'Learning');
-
+		}
 		// "Purpose: Research",
 		//if(d("Purpose: Research",$t))
 		//literal ( $ID, $ns . 'ontology/purpose/research', (v("Purpose: Research",$line)== 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Purpose: Research",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasPurpose', $ns . 'ontology/purpose/research' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasPurpose', $ns . 'ontology/purpose/research' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/purpose/research', 'http://www.w3.org/2000/01/rdf-schema#label', 'Research');
-
+		}
 		// if(d("Scope: Temporal",$t))
 		// literal ( $ID, $ns . 'ontology/scope/temporal', (v("Scope: Temporal",$line) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Scope: Temporal",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/temporal' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/temporal' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/scope/temporal', 'http://www.w3.org/2000/01/rdf-schema#label', 'Temporal');
-
+		}
 		// if(d("Scope: Geographical",$t))
 		// literal ( $ID, $ns . 'ontology/scope/geographical', (v("Scope: Geographical",$line) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Scope: Geographical",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/geographical' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/geographical' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/scope/geographical', 'http://www.w3.org/2000/01/rdf-schema#label', 'Geographical');
-
+		}
 		// if(d("Scope: Genre",$t))
 		// literal ( $ID, $ns . 'ontology/scope/genre', (v("Scope: Genre",$line) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Scope: Genre",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/genre' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/genre' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/scope/genre', 'http://www.w3.org/2000/01/rdf-schema#label', 'Genre');
-
+		}
 		// if(d("Scope: Artist",$t))
 		// literal ( $ID, $ns . 'ontology/scope/artist', (v("Scope: Artist",$line) == 'Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Scope: Artist",$line)) === 'Y') 
-			triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/artist' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://www.w3.org/ns/oa#hasScope', $ns . 'ontology/scope/artist' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/scope/artist', 'http://www.w3.org/2000/01/rdf-schema#label', 'Artist');
-
+		}
 		if(d("Scope: Formats",$t)){
 			$fff = explode(',',v("Scope: Formats",$line ));
 			foreach($fff as $f){
@@ -195,12 +195,14 @@ if ($handle) {
 		// "Access: Public",
 		//literal ( $ID, $ns . 'ontology/access/public', (v("Access: Public",$line)=='Y' ? 'true' : 'false'), 'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Access: Public",$line)) === 'Y') 
-			triple ($ID, 'http://purl.org/dc/terms/accessRights', $ns . 'ontology/access/public' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://purl.org/dc/terms/accessRights', $ns . 'ontology/access/public' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/access/public', 'http://www.w3.org/2000/01/rdf-schema#label', 'Public');
-		if((v("Access: Public",$line)) === 'N') 
-			triple ($ID, 'http://purl.org/dc/terms/accessRights', $ns . 'ontology/access/restricted' ,'uri', 'uri'  );
-			triple ($ns . 'ontology/access/restricted', 'http://www.w3.org/2000/01/rdf-schema#label', 'Restricted');
+		}
 
+		if((v("Access: Public",$line)) === 'N') 
+		{	triple ($ID, 'http://purl.org/dc/terms/accessRights', $ns . 'ontology/access/restricted' ,'uri', 'uri'  );
+			triple ($ns . 'ontology/access/restricted', 'http://www.w3.org/2000/01/rdf-schema#label', 'Restricted');
+		}
 		// "Access: license",
 		triple ( $ID, $ns . 'http://purl.org/dc/terms/license', $ns . md5(v("Access: license",$line  )), 'uri', 'uri');
 		triple ($ns . md5(v("Access: license",$line  )), 'http://www.w3.org/2000/01/rdf-schema#label', v("Access: license",$line  ));
@@ -214,22 +216,22 @@ if ($handle) {
 		//if(d("Interface: Human consumption?",$t))
 		//literal( $ID, $ns . 'ontology/interface/human', (v("Interface: Human consumption?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: Human consumption?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/human-consumption' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/human-consumption' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/human-consumption', 'http://www.w3.org/2000/01/rdf-schema#label', 'Human Consumption');
-
+		}
 		//if(d("Interface: API?",$t))
 		//literal( $ID, $ns . 'ontology/interface/api', (v("Interface: API?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: API?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/api' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/api' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/api', 'http://www.w3.org/2000/01/rdf-schema#label', 'API');
-
+		}
 		// Interface: SPARQL endpoint?
 		//if(d("Interface: SPARQL endpoint?",$t))
 		//literal( $ID, $ns . 'ontology/interface/sparql', (v("Interface: SPARQL endpoint?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: SPARQL endpoint?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/sparql-endpoint' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/sparql-endpoint' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/sparql-endpoint', 'http://www.w3.org/2000/01/rdf-schema#label', 'SPARQL Endpoint');
-
+		}
 		if((v("Interface: SPARQL endpoint?",$line)) === 'Y') 
 			triple ($ID, 'http://rdfs.org/ns/void#sparqlEndpoint', v("SPARQL endpoint URI",$line),'uri', 'uri'  );
 
@@ -237,25 +239,25 @@ if ($handle) {
 		//if(d("Interface: Data Dump?",$t))
 		//literal( $ID, $ns . 'ontology/interface/dump', (v("Interface: Data Dump?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: Data Dump?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/data-dump' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/data-dump' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/data-dump', 'http://www.w3.org/2000/01/rdf-schema#label', 'Data Dump');
-
+		}
 		//if(d("Interface: Is it queryable?",$t))
 		//literal( $ID, $ns . 'ontology/interface/queryable', (v("Interface: Is it queryable?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: Is it queryable?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/query-interface' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/query-interface' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/query-interface', 'http://www.w3.org/2000/01/rdf-schema#label', 'Query Interface');
-
+		}
 		//if(d("Interface: Browsable?",$t))
 		//literal( $ID, $ns . 'ontology/interface/browsable', (v("Interface: Browsable?",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Interface: Browsable?",$line)) === 'Y') 
-			triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/browsing' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://schema.org/featureList', $ns . 'ontology/feature/browsing' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/browsing', 'http://www.w3.org/2000/01/rdf-schema#label', 'Browsing');
-
+		}
 		if(d("Collection: Size",$t))
-		triple( $ID, 'http://purl.org/dc/terms/extent', $ns . md5((v("Collection: Size",$line))),'uri','uri');
-		triple($ns . md5((v("Collection: Size",$line))), 'http://www.w3.org/2000/01/rdf-schema#label', v("Collection: Size",$line));
-		
+		{	triple( $ID, 'http://purl.org/dc/terms/extent', $ns . md5((v("Collection: Size",$line))),'uri','uri');
+			triple($ns . md5((v("Collection: Size",$line))), 'http://www.w3.org/2000/01/rdf-schema#label', v("Collection: Size",$line));
+		}
 		if(d("Data size",$t))
 		triple( $ID, 'http://www.w3.org/ns/dcat#byteSize', (v("Data size",$line)));
 		
@@ -266,51 +268,51 @@ if ($handle) {
 		//if(d("Feature: Melody",$t))
 		//literal( $ID, $ns . 'ontology/feature/melody', (v("Feature: Melody",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Melody",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/melody' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/melody' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/melody', 'http://www.w3.org/2000/01/rdf-schema#label', 'Melody');
-
+		}
 		//if(d("Feature: Harmony",$t))
 		//literal( $ID, $ns . 'ontology/feature/harmony', (v("Feature: Harmony",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Harmony",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/harmony' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/harmony' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/harmony', 'http://www.w3.org/2000/01/rdf-schema#label', 'Harmony');
-
+		}
 		//if(d("Feature: Lyrics",$t))
 		//literal( $ID, $ns . 'ontology/feature/lyrics', (v("Feature: Lyrics",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Lyrics",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/lyrics' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/lyrics' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/lyrics', 'http://www.w3.org/2000/01/rdf-schema#label', 'Lyrics');
-
+		}
 		//if(d("Feature: Rhythm",$t))
 		//literal( $ID, $ns . 'ontology/feature/rhythm', (v("Feature: Rhythm",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Rhythm",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/rhythm' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/rhythm' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/rhythm', 'http://www.w3.org/2000/01/rdf-schema#label', 'Rhythm');
-
+		}
 		//if(d("Feature: Timbre",$t))
 		//literal( $ID, $ns . 'ontology/feature/timbre', (v("Feature: Timbre",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Timbre",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/timbre' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/timbre' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/timbre', 'http://www.w3.org/2000/01/rdf-schema#label', 'Timbre');
-
+		}
 		//if(d("Feature: Contour/Shape",$t))
 		//literal( $ID, $ns . 'ontology/feature/shape', (v("Feature: Contour/Shape",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Contour/Shape",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/contour-or-shape' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/contour-or-shape' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/contour-or-shape', 'http://www.w3.org/2000/01/rdf-schema#label', 'Contour or Shape');
-
+		}
 		//if(d("Feature: Structure",$t))
 		// literal( $ID, $ns . 'ontology/feature/structure', (v("Feature: Structure",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Structure",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/structure' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/structure' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/structure', 'http://www.w3.org/2000/01/rdf-schema#label', 'Structure of a song');
-
+		}
 		//if(d("Feature: Descriptive Metadata",$t))
 		//literal( $ID, $ns . 'ontology/feature/metadata', (v("Feature: Descriptive Metadata",$line)=='Y'?'true':'false'),'http://www.w3.org/2001/XMLSchema#boolean' );
 		if((v("Feature: Descriptive Metadata",$line)) === 'Y') 
-			triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/descriptive-metadata' ,'uri', 'uri'  );
+		{	triple ($ID, 'http://xmlns.com/foaf/0.1/primaryTopic', $ns . 'ontology/feature/descriptive-metadata' ,'uri', 'uri'  );
 			triple ($ns . 'ontology/feature/descriptive-metadata', 'http://www.w3.org/2000/01/rdf-schema#label', 'Descriptive Metadata');
-
+		}
 		// "Situation/Task"
 		if(d("Situation/Task",$t)){
 			$sss = explode(';', v("Situation/Task", $line));
